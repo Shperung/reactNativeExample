6355b11886f83237347d0756630d4634e7b01f86
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2020-02-09 22:09:36 +0200

    Added svg

--- reactNativeExample/src/pages/main/main.screen.js
+++ reactNativeExample/src/pages/main/main.screen.js
@@ -8,11 +8,15 @@ import {
   StatusBar,
 } from 'react-native';
 
+// icons
+import ClosedIcon from '../../svg/assets/closed.svg';
+
 const MainScreen = props => {
   const {navigation} = props;
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>MainScreen</Text>
+      {/*<ClosedIcon width={26} height={26} fill="green" />*/}
       <Button
         title="Charts"
         onPress={() =>
