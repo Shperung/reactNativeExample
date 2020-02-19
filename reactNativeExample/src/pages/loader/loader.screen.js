0f9b08d8460a69e27b90efb4fe9db9846087f01b
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2020-02-19 19:10:47 +0200

    Used react-native-dark-mode

--- reactNativeExample/src/pages/loader/loader.screen.js
+++ reactNativeExample/src/pages/loader/loader.screen.js
@@ -7,13 +7,16 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
+import {useDarkMode} from 'react-native-dark-mode';
 
 const LoaderScreen = props => {
   const {navigation, route} = props;
+  const isDarkMode = useDarkMode();
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>LoaderScreen</Text>
       <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Text>{isDarkMode ? 'dark' : 'light'}</Text>
       <Button
         title="Update the title"
         onPress={() => navigation.setOptions({title: 'Updated!'})}
