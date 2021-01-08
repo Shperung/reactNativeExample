743f6c8579637c25d9cb741290e76d9f08b8ce0d
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2021-01-08 21:50:40 +0200

    refactor

--- catApp63/src/components/toaster/progress.block.tsx
+++ catApp63/src/components/toaster/progress.block.tsx
@@ -4,65 +4,52 @@ import * as Progress from 'react-native-progress';
 
 type Props = {
   isProgress: boolean;
+  duration: number;
+  color: string;
 };
 
+// 1секунда/60герц = 16.6 міллисекунд проходе  заміна кадра. за 16мс і кадр
+const TICK = 1000 / 60;
+// початкове значення прогресу
+const START_PROGRESS = 1;
+
 const ProgressBlock = (props: Props) => {
-  let intreval = null;
-  const {isProgress} = props;
-  const tick = 1000 / 60; // 1секунда/60герц = 16.6 міллисекунд проходе  заміна кадра. за 16мс і кадр
-  // час анімації
-  const duration = 4200;
-
-  // висота для анімації
-  const height = 1;
-
-  const [animatedHeight, setAnimatedHeight] = useState(1);
-
-  const handleAmimated = () => {
-    if (intreval) {
-      // на всякий випадок очищаємо
-      clearInterval(intreval);
-    }
-
-    // задаємо старт анімації
-    let start = Date.now();
-
-    intreval = setInterval(() => {
-      // різниці часу
-      let timePassed = Date.now() - start;
-
-      // якщо різниця часу не виходе за рамки duration то анімуємо
-      if (timePassed <= duration) {
-        // ділимо час що минув на загальний
-        const timeDivide = timePassed / duration;
-        // варіант на сінусах  const curentIndent = Math.sin((timeDivide) * (Math.PI / 2)) * height;
-        // варіант на ізінгові
-        const curentIndent = height - timeDivide;
-
-        // ставлю висоту анімовано
-        setAnimatedHeight(curentIndent);
-      } else {
-        // коли різниця часу зрівнялася з тривалістю то чистимо інтервал і задаємо бажану висоту
-        if (intreval) {
-          clearInterval(intreval);
-        }
-        setAnimatedHeight(0);
-      }
-    }, tick);
-  };
+  const {isProgress, duration = 4000, color = 'rgba(54, 54, 54, 0.5)'} = props;
+  const [animatedProgress, setAnimatedProgress] = useState(1);
 
   useEffect(() => {
-    if (isProgress) handleAmimated();
+    let intreval = null;
+    if (isProgress) {
+      // задаємо старт анімації
+      let start = Date.now();
+
+      intreval = setInterval(() => {
+        // різниці часу
+        let timePassed = Date.now() - start;
+        // якщо різниця часу не виходе за рамки duration то анімуємо
+        if (timePassed <= duration) {
+          // ділимо час що минув на загальний
+          const timeDivide = timePassed / duration;
+          // анімуємо значеннявід 1-0
+          const curentValue = START_PROGRESS - timeDivide;
+          setAnimatedProgress(curentValue);
+        } else {
+          // коли різниця часу зрівнялася з тривалістю  задаємо прогрес 0
+          setAnimatedProgress(0);
+        }
+      }, TICK);
+    }
+
+    return () => (intreval ? clearInterval(intreval) : null);
   }, [isProgress]);
 
   return (
     <Progress.Circle
       size={30}
-      progress={animatedHeight}
+      progress={animatedProgress}
       direction="counter-clockwise"
-      color="rgba(54, 54, 54, 0.5)"
+      color={color}
       borderWidth={0}
-      animated
       strokeCap="round"
       thickness={5}
     />
